202e2a455e0f360f1111b493b4153a9c50f6d552
TaNSiRa <tansirawit2543@gmail.com>
2025-10-08 17:22:04 +0700

    .

--- flow/001/03SENDMAIL.js
+++ flow/001/03SENDMAIL.js
@@ -378,15 +378,15 @@ function generateSummaryStats(statusMap) {
 
     return `
     <div class="summary-stats">
-      <div class="stat-card" style ="color: blue;">
+      <div class="stat-card">
         <div class="stat-number">${totalShipments}</div>
         <div class="stat-label">Total Shipments</div>
       </div>
-      <div class="stat-card" style ="color: orange;>
+      <div class="stat-card">
         <div class="stat-number">${totalStatuses}</div>
         <div class="stat-label">Active Statuses</div>
       </div>
-      <div class="stat-card" style ="color: green;>
+      <div class="stat-card">
         <div class="stat-number">${totalComplete}</div>
         <div class="stat-label">Complete Statuses</div>
       </div>
